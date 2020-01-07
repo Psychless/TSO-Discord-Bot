0ca39c5a366e61be3bb12b0a154cb646a84bde4f
kristaps.baumanis <kristaps.baumanis@autentica.lv>
2020-01-07 17:17:10 +0200

    Update  command for squad roles

--- commands/setrep.js
+++ commands/setrep.js
@@ -14,28 +14,41 @@ exports.run = (client, message, args, config) => {
         }
 
         if (args.length !== 2) {
-            return message.channel.send(`Usage: ${config.cmdkey}setrep @user number`);
+            return message.channel.send(`Usage: ${config.cmdkey}setrep @user/@role number`);
         }
 
         const user = message.mentions.users.first();
-        if (!user) {
-            return message.channel.send(`Usage: ${config.cmdkey}setrep @user number`);
+        const role = message.mentions.roles.first();
+        if (!user && !role) {
+            return message.channel.send(`Usage: ${config.cmdkey}setrep @user/@role number`);
         }
 
         const number = parseInt(args[1]);
         if (isNaN(number)) {
-            return message.channel.send(`Usage: ${config.cmdkey}setrep @user number`);
+            return message.channel.send(`Usage: ${config.cmdkey}setrep @user/@role number`);
         }
-
-        const rep = JSON.parse(fs.readFileSync(`./rep.json`));
-        if (!rep[user.id]) {
-            rep[user.id] = 0;
+        
+        if (user) {
+            const rep = JSON.parse(fs.readFileSync(`./rep.json`));
+            if (!rep[user.id]) {
+                rep[user.id] = 0;
+            }
+    
+            rep[user.id] = number;
+    
+            message.channel.send(`Set ${user.username}'s rep to ${number}.`);
+            fs.writeFileSync(`./rep.json`, JSON.stringify(rep, null, 4));
         }
-
-        rep[user.id] = number;
-
-        message.channel.send(`Set ${user.username}'s rep to ${number}.`);
-
-        fs.writeFileSync(`./rep.json`, JSON.stringify(rep, null, 4));
+        else {
+            const rep = JSON.parse(fs.readFileSync(`./rolerep.json`));
+            if (!rep[role.id]) {
+                rep[role.id] = 0;
+            }
+    
+            rep[role.id] = number;
+    
+            message.channel.send(`Set ${role.name}'s rep to ${number}.`);
+            fs.writeFileSync(`./rolerep.json`, JSON.stringify(rep, null, 4));
+        }
     });    
 }
